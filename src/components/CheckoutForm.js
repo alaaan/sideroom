8d7f62735147a1c07bf92a9c1a60d17ab4b83a75
Alan Sher <alan@conectr.io>
2020-12-29 11:14:53 -0500

    update checkout form

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -17,9 +17,9 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import 'yup-phone';
 
-
-const validationSchema = yup.object({
+const defaultValidationSchema = yup.object({
   email: yup
     .string('Enter your email')
     .email('Enter a valid email')
@@ -30,14 +30,21 @@ const validationSchema = yup.object({
     .required('Name is required!'),
 
   phone: yup
-    .string('Enter your phone')
+    .string()
+    .phone("US")
     .required('Enter your phone number'),
 
   request: yup
     .string('Tell us a little bit about your request')
     .min(20, 'Tell us a little bit more!')
-    .required('Please tell us about your request')
+    .required('Please tell us about your request'),
+
+  date: yup
+    .string('whats up with a date fam')
+    .required('Please select a date'),
 });
+
+
 
 const MyField = withStyles({
   root: {
@@ -193,143 +200,20 @@ const CheckoutButton = withStyles({
   },
 })(Button);
 
-const renderForm = (formik) => {
-
-
-  return (
-    <form onSubmit={formik.handleSubmit} id='checkout-form' key='checkout-form'>
-      <h2 style={{ marginLeft: '10px', marginTop: '10px' }}>Your Info</h2>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <MyField
-          fullWidth
-          id="name"
-          name="name"
-          label="Name"
-          variant="outlined"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          error={formik.touched.name && Boolean(formik.errors.name)}
-          helperText={formik.touched.name && formik.errors.name}
-        />
-        {/* <MyField label="Name" key="name-input" value={name} variant="outlined" onChange={handleName} error={nameError} /> */}
-        {/* <MyField label="Email" variant="outlined" /> */}
-        <MyField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          variant="outlined"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-
-        {/* <MyField label="Phone" variant="outlined" /> */}
-        <MyField
-          id="phone"
-          name="phone"
-          label="Phone"
-          variant="outlined"
-          value={formik.values.phone}
-          onChange={formik.handleChange}
-          error={formik.touched.phone && Boolean(formik.errors.phone)}
-          helperText={formik.touched.phone && formik.errors.phone}
-        />
-
-        <CheckoutButton>Confirm Phone</CheckoutButton>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignContent: 'center', marginTop: '5px', marginLeft: '10px' }}>
-
-        <h2 style={{ marginTop: '10px' }}>Call Info</h2>
-        <h3 style={{ marginTop: '10px' }}>Tell the host all about your request!</h3>
-        <h4>Is it your birthday? Whats popping?</h4>
-        {/* <FormField multiline rows={3} variant="outlined" /> */}
-        <FormField
-          multiline
-          rows={3}
-          id="request"
-          name="request"
-          variant="outlined"
-          value={formik.values.request}
-          onChange={formik.handleChange}
-          error={formik.touched.request && Boolean(formik.errors.request)}
-          helperText={formik.touched.request && formik.errors.request}
-        />
-
-        <h3 style={{ marginTop: '20px' }}>What is your preferred date for a call? (no promises😜)</h3>
-        {/* <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <SRDatePicker
-            margin="normal"
-            inputVariant="outlined"
-            id="date-picker-dialog"
-            format="MM/dd/yyyy"
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-        </MuiPickersUtilsProvider>
-        <div style={{ display: 'flex', flexDirection: 'row', marginTop: '20px', alignItems: 'center' }}>
-          <h3>Is this a gift for someone else?</h3>
-          <SRFormControlLabel
-            control={<Checkbox style={{ marginLeft: '20px' }} checked={giftChecked} onChange={handleGiftChecked} name="gift" />}
-            label="Yes"
-          />
-        </div>
-
-        {giftChecked &&
-          <div>
-            <h2 style={{ marginTop: '10px' }}>Gift Info</h2>
-            <MyField label="Recipient Name" variant="outlined" />
-            <MyField label="Recipient Phone" variant="outlined" />
-          </div>}
-
-        <h2 style={{ marginTop: '10px' }}>Payment Details</h2>
-        {/* <MyField label="Credit Card" variant="outlined" />
-        <div style={{ display: 'flex', flexDirection: 'row' }}> */}
-        {/* <CardElement
-          id="card-element"
-          options={CARD_ELEMENT_OPTIONS}
-          onChange={handleCardChange}
-          className="card-element"
-        /> */}
-        {/* <MyField label="Expiration" variant="outlined" />
-          <MyField label="CVV" variant="outlined" />
-          <MyField label="Zip Code" variant="outlined" /> */}
-        {/* </div> */}
-        <CheckoutButton type="submit" style={{ marginTop: '40px', marginBottom: '20px' }}>PURCHASE CALL</CheckoutButton> */}
-
-      </div>
-    </form>
-
-  )
-}
 
 
 const CheckoutForm = (props) => {
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      name: '',
-      phone: '',
-      request: ''
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const [giftChecked, toggleGiftCheked] = useCycle(false, true);
+  const [selectedDate, setSelectedDate] = useState(Date.now());
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    formik.values.date = date;
+  };
 
   const stripe = useStripe();
   const elements = useElements();
-
-  const [selectedDate, setSelectedDate] = useState(Date.now());
-  const [giftChecked, toggleGiftCheked] = useCycle(false, true);
+  const cardElement = elements.getElement(CardElement);
 
   //stripe stuff
   const CARD_ELEMENT_OPTIONS = {
@@ -358,13 +242,6 @@ const CheckoutForm = (props) => {
     },
   };
 
-  //handlers
-
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
   const handleCardChange = (event) => {
     // if (event.error) {
     //   setError(event.error.message);
@@ -376,27 +253,157 @@ const CheckoutForm = (props) => {
     // setHasCompleteCard(event.complete);
   };
 
-
   const handleGiftChecked = () => {
     toggleGiftCheked();
   };
 
-  const variants = {
-    open: { opacity: 1, transition: { staggerChildren: 1 } },
-    closed: { opacity: 0 }
-  }
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name: '',
+      phone: '',
+      request: '',
+      date: '',
+      recipientName: ''
+    },
+    validationSchema: defaultValidationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-  const childVariants = {
-    open: { opacity: 1 },
-    closed: { opacity: 0 }
-  }
+  const today = new Date();
+
+
+
   return (
     <div>
       <CloseIcon
         onClick={props.toggle}
         fontSize='large' color="secondary" style={{ position: 'absolute', top: 5, right: 5 }} />
 
-      {renderForm(formik)}
+      <form onSubmit={formik.handleSubmit} id='checkout-form' key='checkout-form'>
+        <h2 style={{ marginLeft: '10px', marginTop: '10px' }}>Your Info</h2>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <MyField
+            fullWidth
+            id="name"
+            name="name"
+            label="Name"
+            variant="outlined"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+          />
+          {/* <MyField label="Name" key="name-input" value={name} variant="outlined" onChange={handleName} error={nameError} /> */}
+          {/* <MyField label="Email" variant="outlined" /> */}
+          <MyField
+            fullWidth
+            id="email"
+            name="email"
+            label="Email"
+            variant="outlined"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+
+          {/* <MyField label="Phone" variant="outlined" /> */}
+          <MyField
+            id="phone"
+            name="phone"
+            label="Phone"
+            variant="outlined"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            error={formik.touched.phone && Boolean(formik.errors.phone)}
+            helperText={formik.touched.phone && formik.errors.phone}
+          />
+
+          <CheckoutButton>Confirm Phone</CheckoutButton>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignContent: 'center', marginTop: '5px', marginLeft: '10px' }}>
+          <h2 style={{ marginTop: '10px' }}>Call Info</h2>
+          <h3 style={{ marginTop: '10px' }}>Tell the host all about your request!</h3>
+          <h4>Is it your birthday? Whats popping?</h4>
+          <FormField
+            multiline
+            rows={3}
+            id="request"
+            name="request"
+            variant="outlined"
+            value={formik.values.request}
+            onChange={formik.handleChange}
+            error={formik.touched.request && Boolean(formik.errors.request)}
+            helperText={formik.touched.request && formik.errors.request}
+          />
+
+          <h3 style={{ marginTop: '20px' }}>What is your preferred date for a call? (no promises😜)</h3>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <SRDatePicker
+              minDate={today}
+              margin="normal"
+              inputVariant="outlined"
+              id="date-picker-dialog"
+              format="MM/dd/yyyy"
+              value={formik.values.date}
+              onChange={handleDateChange}
+              error={formik.touched.date && Boolean(formik.errors.date)}
+              helperText={formik.touched.date && formik.errors.date}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row', marginTop: '20px', alignItems: 'center' }}>
+          <h3>Is this a gift for someone else?</h3>
+          <SRFormControlLabel
+            control={<Checkbox style={{ marginLeft: '20px' }} checked={giftChecked} onChange={handleGiftChecked} name="gift" />}
+            label="Yes"
+          />
+        </div>
+
+        {giftChecked &&
+          <div>
+            <h2 style={{ marginTop: '10px' }}>Gift Info</h2>
+            <MyField
+              id="recipientName"
+              name="recipientName"
+              label="Recipient Name"
+              variant="outlined"
+              value={formik.values.recipientName}
+              onChange={formik.handleChange}
+              error={formik.touched.recipientName && Boolean(formik.errors.recipientName)}
+              helperText={formik.touched.recipientName && formik.errors.recipientName}
+            />
+            <MyField label="Recipient Phone" variant="outlined" />
+          </div>}
+
+        <h2 style={{ marginTop: '10px' }}>Payment Details</h2>
+        <MyField label="Credit Card" variant="outlined" />
+
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <CardElement
+            id="card-element"
+            options={CARD_ELEMENT_OPTIONS}
+            onChange={handleCardChange}
+            className="card-element"
+          />
+          <MyField label="Expiration" variant="outlined" />
+          <MyField label="CVV" variant="outlined" />
+          <MyField label="Zip Code" variant="outlined" /> */}
+      </div>
+        <CheckoutButton type="submit" style={{ marginTop: '40px', marginBottom: '20px' }}>PURCHASE CALL</CheckoutButton> */}
+
+
+    </form >
 
     </div>
 
