28584374c8c561863dc7bfcb16e0f179d58f7374
Alan Sher <alan@conectr.io>
2021-01-12 21:19:01 -0500

    Mobile responsive host detail page

--- src/components/SRPriceButton.js
+++ src/components/SRPriceButton.js
@@ -1,13 +1,35 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import SRButton from '../components/SRButton'
-import { render } from 'react-dom';
+import { css, jsx} from "@emotion/react";
+
 
 const SRPriceButton = ({clickHandler})=>{
 
   return (
-    <div className="price-button-container">
-      <div className="price-button-price">
-      <h2>$400</h2>
+    <div css={css`
+    position:relative; 
+    width: 70%;`}>
+      <div
+      className='price-button-price'
+      css={css`
+        border: 4px solid white;
+        background:var(--darkblue);
+        width:75px;
+        height:75px;
+        display:flex; 
+        flex-direction:column; 
+        justify-content:center; 
+        align-items:center; 
+        border-radius:50%; 
+        max-width: 90px;
+        max-height: 90px;
+        position: absolute;
+        top: -10%;
+        z-index: 100;`}>
+
+      <h2 
+      css={css`font-size: 1.5rem;`}>$400</h2>
       </div>
       <SRButton onClick={clickHandler}>Let's do this</SRButton>
     </div>
