c413ed2358716a3bdfe4582bbd9edb419daf8c99
Alan Sher <alan@conectr.io>
2021-01-26 16:20:55 -0500

    change animation on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,9 +8,6 @@ import waiting from '../img/waiting.png'
 import step4 from '../img/step4.png';
 import Header from '../components/Header';
 import door from '../img/door.png';
-
-
-
 import {
   useRef,
   useEffect
@@ -22,7 +19,9 @@ import Phone from '../components/phone'
 import Sofa from '../img/sofa.png';
 import HostCard from '../components/HostCard'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ReactPlayer from 'react-player';
 
+import {ReactComponent as PhoneSvg} from '../img/phone.svg'
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
@@ -53,31 +52,84 @@ function App() {
 
     //setup animations here
 
-    // let tl = gsap.timeline({
-    //   smoothChildTiming: true,
-    // });
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".how-to-section",
+        markers:true,
+        //pin: true,   // pin the trigger element while active
+        pinSpacing:true,
+        pin:'.how-to-section',
+        start: "top top", // when the top of the trigger hits the top of the viewport
+        end: "+=5000", // end after scrolling 5000px beyond the start
+        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        // snap: {
+        //   snapTo: "labels", // snap to the closest label in the timeline
+        //   duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+        //   delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+        //   ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+        // }
+        }
 
-    // // tl.to('.phone', 3, { rotation: "360", ease: 'none' });
-    // // tl.to('.phone', 1, { opacity: 0, display: 'none' });
-    // tl.to('.sofa', 3, { rotation: "360", ease: 'none', opacity: 0, display: 'none' });
-    // // tl.fromTo('.phone', .5, { xPercent: 200 }, { xPercent: 0 })
-    // tl.to('.loading-text', { text: 'Are you ready to meet them?', duration: 2, opacity: 1 })
-    // // tl.fromTo('.loading-text', { y: -100 }, { y: 0, opacity: 1 })
-    // // tl.to('.loading-text', { y: 25, zIndex: 0, delay: 2 })
-    // tl.to('.section-1', .1, { opacity: 1, delay: 2 })
-    // tl.fromTo('.section-1', { y: -1000 }, { y: 0, ease: 'Bounce.easeOut', duration: 2 }, '-=.5')
-    // tl.from('.hero-text', { y: 10, opacity: 0, delay: 1 })
+    });
 
-    // let tl2 = gsap.timeline();
+      // .to(".test-thing-1", {opacity:1})
+      tl.addLabel("start")
+      .to(".test-thing-1", {opacity:1,scale:1.2})
+      .to(".test-thing-1", {scale:1,opacity:0,autoAlpha:true})
+      tl.addLabel("second step")
+      .to('.test-thing-2',{scale:1.2,opacity:1})
+      .to('.test-thing-2',{opacity:0,scale:1})
+      tl.addLabel("third step")
+      .to('.test-thing-3',{scale:1.2,opacity:1})
+      .to('.test-thing-3',{opacity:0,scale:1})
+  
 
-    // tl2.to(('.get-call-headline'),
-    //   {
-    //     text: "Pick your host to get started! ",
-    //     duration: 1,
-    //     scrollTrigger: { id: 'get-call-id', trigger: '.section-2', start: 'top center', toggleActions: 'play none none none' }
-    //   });
-    // tl2.from('.get-call-subheading', { y: 10, opacity: 0, delay: 1 })
 
+    // tl.to('.test-thing-1',{
+    // scrollTrigger: {
+    //   trigger: ".how-to-section",
+    //   markers:true,
+    //   //pin: true,   // pin the trigger element while active
+    //   pinSpacing:true,
+    //   pin:'.how-to-section',
+    //   start: "top top", // when the top of the trigger hits the top of the viewport
+    //   end: "+=500", // end after scrolling 500px beyond the start
+    //   scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    //   },opacity:1},1);
+
+      // tl.to('.test-thing-1',{
+      //   scrollTrigger: {
+      //     trigger: ".how-to-section",
+      //     markers:true,
+      //     //pin: true,   // pin the trigger element while active
+      //     pinSpacing:true,
+      //     pin:'.how-to-section',
+      //     start: "top +=500", // when the top of the trigger hits the top of the viewport
+      //     end: "+=500", // end after scrolling 500px beyond the start
+      //     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      //     },opacity:0},1);
+
+      // tl.to('.test-thing-2',{
+      //   scrollTrigger: {
+      //     trigger: ".test-thing-2",
+      //     markers:true,
+      //     pin: true,   // pin the trigger element while active
+      //     pinSpacing:true,
+      //     start: 2000, // when the top of the trigger hits the top of the viewport
+      //     end: "+=500", // end after scrolling 500px beyond the start
+      //     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      //     },opacity:1},1);
+
+      let headerTl=gsap.timeline();
+      headerTl.addLabel('start');
+      headerTl.from('.headline-h1',{transform:'skew(0deg)',repeat:3,yoyo:true,duration:.5,ease:'Power2.out'},'start'); 
+      headerTl.from('.hero-secondary-text',{opacity:0,duration:.5},'start+=1')
+      headerTl.staggerFrom('.phoneSVG path',.5,{opacity:0,stagger:.05},'start+=1'); 
+      headerTl.staggerFrom('.phoneSVG rect',.5,{opacity:0,stagger:.05},'start+=1'); 
+      headerTl.staggerFrom('.phoneSVG circle',.5,{opacity:0,stagger:.05,y:100},'start+=1'); 
+
+
+      
   }, []);
 
   const dummyHosts = [
@@ -143,26 +195,30 @@ function App() {
         </section> */}
         <section className="hero-section">
           <div className="hero-left-container" style={{ gridRow: 2, gridColumn: 2 }}>
-            <h1>Get a call from your favorite celeb.</h1>
+            <h1 className='headline-h1'>Get a call from your favorite celeb.</h1>
             <h2 className="hero-secondary-text">Meet them in the sideroom.</h2>
           </div>
-          <img src={hero} style={{ gridRow: 2, gridColumn: 3 }} />
+          <PhoneSvg className='phoneSVG' style={{ gridRow: 2, gridColumn: 3, justifySelf:'center' }} />
+  
         </section>
 
-        <section className="host-section">
-          <div style={{ gridRow: 2, gridColumn: 2 }}>
-            <h1 className="get-call-headline">Ready for a call?</h1>
-            <h2 className="host-secondary-text">Pick your host to get started.</h2>
-          </div>
-          <div className="host-grid" style={{ gridRow: 3, gridColumnStart: 2, gridColumnEnd: 4 }}>
-            <HostCards hosts={dummyHosts} />
-          </div>
-
-        </section>
+        
         <section className="how-to-section">
-          <div style={{ gridRow: 2, gridColumn: 2 }}>
+          <div className='test-thing-1'>
+            <h1>Purchase Graphic</h1>
+          </div>
+          <div className='test-thing-2'>
+          <h2>second step fam</h2>
+            <ReactPlayer width='auto' loop muted playing playsinline url='https://conectrmedia.blob.core.windows.net/files/testfacetime.mp4' />
+          </div>
+          <div className='test-thing-3'>
+            <h2>Third step fam</h2>
+            <ReactPlayer width='auto' loop muted playing playsinline url='https://conectrmedia.blob.core.windows.net/files/testfacetime.mp4' />
+
+          </div>
+          {/* <div style={{ gridRow: 2, gridColumn: 2 }}>
             <h1 className="get-call-headline">How does this work?</h1>
-            <h2 className="hero-secondary-text">We got you covered.</h2>
+            <h2 className="hero-seco%;dary-text">We got you covered.</h2>
           </div>
           <div className="how-to-grid">
             <div className="step-container">
@@ -183,8 +239,22 @@ function App() {
                 <StepImage src={step4} />
               </div>
             </div>
-          </div>
+          </div> */}
         </section>
+
+        <section className="host-section">
+          <div style={{ gridRow: 2, gridColumn: 2 }}>
+            <h1 className="get-call-headline">Ready for a call?</h1>
+            <h2 className="host-secondary-text">Pick your host to get started.</h2>
+          </div>
+          <div className="host-grid" style={{ gridRow: 3, gridColumnStart: 2, gridColumnEnd: 4 }}>
+            <HostCards hosts={dummyHosts} />
+          </div>
+
+        </section>
+        <section className="footer">
+        </section>
+
 
 
       </div>
