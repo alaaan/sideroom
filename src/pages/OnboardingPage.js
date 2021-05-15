7a072e7260999ffdd7535ee8cf7db3bbc8930de2
Alan Sher <alan@conectr.io>
2021-05-14 20:02:04 -0400

    onboarding form validation

--- src/pages/OnboardingPage.js
+++ src/pages/OnboardingPage.js
@@ -6,6 +6,7 @@ import '../styles/OnboardingPage.css'
 import infoIcon from '../img/info-icon.svg'
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
+import * as yup from 'yup';
 
 import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
@@ -27,10 +28,58 @@ registerPlugin(FilePondPluginFileEncode,FilePondPluginFileValidateType,
   FilePondPluginImageResize,FilePondPluginImageExifOrientation,FilePondPluginImagePreview,
   FilePondPluginImageCrop); 
 
+  // const defaultValidationSchema = yup.object({
+  
+  //   name: yup
+  //     .string('Enter your name')
+  //     .required('Name is required!'),
+  //   request: yup
+  //     .string('Tell us a little bit about your request')
+  //     .min(3, 'Tell us a little bit more!')
+  //     .required('Please tell us about your request'),
+  // });
+  
+
+  const onboardingSchema = yup.object({
+    slug: yup
+      .string('enter your slug')
+      .required('a listing url is required'),
+    name: yup
+      .string()
+      .required('a name is required'),
+    username: yup
+      .string()
+      .required('a username is required'),
+    password: yup
+      .string()
+      .required('a password is required.') 
+      .min(8, 'password must be at least 8 characters.'),
+    passwordConfirmation: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'passwords must match'),
+    price: yup
+      .number()
+      .required()
+      .min(5,'your price must be at least $5 per call'),
+    minutes: yup
+      .number()
+      .required()
+      .min(2,'your calls must be at least 2 minutes'),
+    slots: yup
+      .number()
+      .required()
+      .positive(),
+    description: yup
+      .string()
+      .min(5,'you must have a listing description')
+      
+     });
 
 
 
 const OnboardingPage = () =>{
+
+
 
   const {setHasHeader} = useContext(ThemeContext);
   const [showImg,setShowImg] = useState(false);
@@ -39,19 +88,17 @@ const OnboardingPage = () =>{
   const [isUploading,setIsUploading]=useState(false);
 
   const [accessCode,setAccessCode] = useState('testcode');
+  const [slug,setSlug]=useState(null);
   const [username,setUsername] = useState(null);
   const [password,setPassword] = useState(null);
-  const [passwordCheck,setPasswordCheck] = useState(null);
+  const [passwordConfirmation,setPasswordConfirmation] = useState(null);
   const [name,setName] = useState(null);
   const [profileImgUrl,setProfileImgUrl] = useState(''); 
-  const [listingPrice,setListingPrice] = useState(null);
-  const [listingDescription,setListingDescription] = useState(null);
-  const [maxSlots,setMaxSlots] = useState(null);
-  const [time,setTime] = useState(null);
+  const [price,setPrice] = useState(null);
+  const [description,setDescription] = useState(null);
+  const [slots,setSlots] = useState(null);
+  const [minutes,setMinutes] = useState(null);
   
-
-  const imgLoaded = useImageLoader(profileImgUrl);
-
 
   const service = new UserWebService(); 
 
@@ -61,18 +108,43 @@ const OnboardingPage = () =>{
 
   const submitOnboard = async ()=>{
 
-    const result = await service.onboard(accessCode,username,password,name,profileImgUrl,listingPrice,listingDescription,maxSlots,time);
-    
-    if (!result.Errored) {
-    
-      //success
-
+    //create form data 
+    let formData = {
+      slug: slug,
+      name: name,
+      username: username,
+      password: password,
+      passwordConfirmation: passwordConfirmation,
+      price: price,
+      minutes: minutes,
+      slots: slots,
+      description: description
     }
 
-    else{
-      //error
+    //validation
 
-    }
+    onboardingSchema.validate(formData, { abortEarly: false })
+    .then(function() {})
+    .catch(function (err) {
+        err.inner.forEach(e => {console.log(e.message)});});
+
+    // const isValid = await onboardingSchema.isValid(formData).catch(function (err) {
+    //   console.log(err); // => 'ValidationError'
+    // });
+    // console.log(formData);
+    // console.log(isValid);
+    // const result = await service.onboard(accessCode,username,password,name,profileImgUrl,listingPrice,listingDescription,maxSlots,time);
+    
+    // if (!result.Errored) {
+    
+    //   //success
+
+    // }
+
+    // else{
+    //   //error
+
+    // }
 
 
   }
@@ -143,7 +215,7 @@ const OnboardingPage = () =>{
           <p>This is where fans will go to purchase a call</p>
           <div>
             <h3 style={{display:'inline'}}>connect.fans/</h3>
-            <input className='onboard-input slug' type='text' placeholder='michaeljordan' onChange={e => setName(e.target.value)} />
+            <input className='onboard-input slug' type='text' placeholder='michaeljordan' onChange={e => setSlug(e.target.value)} />
           </div>
         </section>
 
@@ -170,28 +242,28 @@ const OnboardingPage = () =>{
             
           <div className="input-stack">
             <p className='input-label'>Confirm Password</p>
-            <input className='onboard-input' type='password' onChange={e => setPasswordCheck(e.target.value)} />
+            <input className='onboard-input' type='password' onChange={e => setPasswordConfirmation(e.target.value)} />
           </div>
 
           <div className="input-stack">
             <p className='input-label'>Price per call</p>
-            <input className='onboard-input' type='text' placeholder='$100' onChange={e => setListingPrice(e.target.value)} />
+            <input className='onboard-input' type='text' placeholder='$100' onChange={e => setPrice(e.target.value)} />
           </div>
 
           <div className="input-stack">
             <p className='input-label'>Minutes per call</p>
-            <input className='onboard-input' type='text' placeholder='2 minutes' onChange={e => setTime(e.target.value)} />
+            <input className='onboard-input' type='text' placeholder='2 minutes' onChange={e => setMinutes(e.target.value)} />
           </div>
 
           <div className="input-stack">
             <p className='input-label'># of available</p>
-            <input className='onboard-input' type='text' placeholder='slots available' onChange={e => setMaxSlots(e.target.value)} />
+            <input className='onboard-input' type='text' placeholder='slots available' onChange={e => setSlots(e.target.value)} />
           </div>
 
           <div className="input-stack">
             <p className='input-label'>Describe your public listing</p>
             {/* <img src={infoIcon} alt='info' /> */}
-            <textarea className='onboard-input text-area-input' cols="40" rows="5" type='text' placeholder='Im excited to meet you. In your request, tell me what you want to talk about.' onChange={e => setListingDescription(e.target.value)} />
+            <textarea className='onboard-input text-area-input' cols="40" rows="5" type='text' placeholder='Im excited to meet you. In your request, tell me what you want to talk about.' onChange={e => setDescription(e.target.value)} />
           </div>
           
          
